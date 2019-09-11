1a264093e887adc1ff9baa1db3675254893a64b5
limchangmin95 <53820222+limchangmin95@users.noreply.github.com>
2019-09-11 16:25:56 +0900

    --0911 16:25 workspace switch
    --0911 16:25 workspace switch

---
+++ ChanMin/workspace/GoodDeV/src/main/java/com/dev/cloud/vo/PTI.java
@@ -0,0 +1,15 @@
+package com.dev.cloud.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PTI {
+
+private int  PTI_seq ;
+private int  itemNum ;
+
+}
