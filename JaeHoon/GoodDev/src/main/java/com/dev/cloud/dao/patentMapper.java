a88a4c55d7d95c858ec33bb1a199e7f485b45665
ytseerr7 <53821386+ytseerr7@users.noreply.github.com>
2019-09-21 01:25:29 +0900

    officialFile테이블 생성 및 mapper, 패턴트 셀렉(조건별)

--- JaeHoon/GoodDev/src/main/java/com/dev/cloud/dao/patentMapper.java
+++ JaeHoon/GoodDev/src/main/java/com/dev/cloud/dao/patentMapper.java
@@ -1,17 +1,24 @@
 package com.dev.cloud.dao;
 
+import java.util.ArrayList;
+
 import com.dev.cloud.vo.Patent;
 
 public interface patentMapper {
-	
-	//특허 생성
+
+	// 특허 생성
 	public int insertPatent(Patent patent);
-	
-	//특허 승인(update)
+
+	// 특허 승인(update)
 	public int updatePatent(Patent patent);
-	
-	
-	
-	
-	
+
+	// 특허 리스트
+	public ArrayList<Patent> patentAll();
+
+	// 멤버이름(특허등록자 이름)으로 셀렉트올
+	public ArrayList<Patent> patentIdAll(String memberName);
+
+	// 분류별, 검색어별 셀렉트
+	public ArrayList<Patent> searchPatent(Patent patent);
+
 }
