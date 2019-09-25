a775607f356221ddda2bcb57c990ed106f34d711
limchangmin95 <53820222+limchangmin95@users.noreply.github.com>
2019-09-25 23:46:28 +0900

    -- 190925 middleSide_ patent_useList, patent_useList에서 download 전까지 만들기
    -- 190925 middleSide_ patent_useList, patent_useList에서 download 전까지 만들기

--- ChanMin/workspace/GoodDeV/src/main/java/com/dev/cloud/dao/PtiRepository.java
+++ ChanMin/workspace/GoodDeV/src/main/java/com/dev/cloud/dao/PtiRepository.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.dev.cloud.vo.ItemDo;
+import com.dev.cloud.vo.PTI;
 
 @Repository
 public class PtiRepository implements PTIMapper {
@@ -32,6 +33,20 @@ public class PtiRepository implements PTIMapper {
 		PTIMapper mapper=session.getMapper(PTIMapper.class);
 		try {
 			result=mapper.updatePTI(itemdo);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	@Override
+	public PTI selectPti(int PTI_seq) {
+		PTI result= null;
+		PTIMapper mapper=session.getMapper(PTIMapper.class);
+		try {
+			result=mapper.selectPti(PTI_seq);
 
 		} catch (Exception e) {
 			e.printStackTrace();
